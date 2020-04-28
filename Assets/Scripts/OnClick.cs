e0194496b4fed7eb07e5155ee46e0abde0d9755d
Микола Мантицький <slwpke447@gmail.com>
2020-04-28 22:55:18 +0300

    create

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnClick : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("Exit pressed!");
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void ExitPressed()
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+
+    }
+    public void PlayAgainPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+}
